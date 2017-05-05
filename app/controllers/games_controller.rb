0d2eb3578394683ba456b7dcf786ccfb808b5245
emilyjf <flanagan.emily.j@gmail.com>
2017-05-04 21:28:01 -0500

    Thursday

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -28,6 +28,39 @@ class GamesController < ApplicationController
       else
         @answer_message = "Right on the money!"
       end
+
+      def form_show
+
+      end
+
+      def form_send
+        @message = params["form_message"]
+      end
+
+      def form_guess
+
+      end
+
+      def form_guess_send
+        @guess = params["guess"]
+      end
+
+        
+      end
+
+      def segment_guess
+        @user_guess = params["turkey"].to_i
+        winning_number = 42
+
+      if winning_number < @user_guess
+        @answer_message = "too high."
+      elsif winning_number > @user_guess
+        @answer_message = "too low."
+      else 
+        @answer_message = "right on the money!
+      end
     end
   end
+
+    render 'form_guess.html.erb'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
-  #get '/games_app' => 'games#games_app'#
+  get '/games_app' => 'games#games_app'#
   get '/url_segment_params/:purple_hippo' => 'games#url_segment'
+end
+
+get 'form_show_params' => 'games#form_show'
+
+post '/form_send_params' => 'games#form_send'
+
+
+
+
+
+  get 'name' => 'games#name'
+  get 'guess' => 'games#guess'
+  get 'segment_guess/:turkey' => 'games#segment_guess'
+
+  get '/form_guess' => 'games#form_guess'
+  post '/form_guess_send' => 'games#form_guess_send'
 end
